c53029a9d1256107c0cc32bdad4a04e797101e9a
shubhajeet <shubha.pradhan12@gmail.com>
2022-08-22 18:10:54 +0530

    colour issue fixed 🛠

--- ascify.py
+++ ascify.py
@@ -1,33 +1,29 @@
 import cv2
 import numpy as np
 from PIL import Image, ImageDraw, ImageOps, ImageFont
+import os
 
-Character = {
+Characters = {
     "standard": "@%#*+=-:. ",
     "complex": "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
 }
 
 
 def get_data(mode):
-    font = ImageFont.truetype("./fonts/DejaVuSansMono.ttf", size=20)
-    scale = 2
-    char_list = Character[mode]
-    return char_list, font, scale
-
-
-bg = "black"
-if bg == "black":
-    bg_code = (255, 255, 255)
-elif bg == "white":
-    bg_code = (0, 0, 0)
-
-(char_list, font, scale) = get_data("complex")
-num_chars = len(char_list)
-num_cols = 300
+    font_filename = "DejaVuSansMono"
+    font = ImageFont.truetype(os.path.dirname(__file__) + "/fonts/" + font_filename + ".ttf", size=20)
+    char_list = Characters[mode]
+    return char_list, font
 
 
 def convert(img_loc):
+    (char_list, font) = get_data("complex")
+    num_chars = len(char_list)
+    num_cols = 300
+    scale = 2
+
     image = cv2.imread(img_loc)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     height, width, garbage = image.shape
 
@@ -39,23 +35,23 @@ def convert(img_loc):
     out_width = char_width * num_cols
     out_height = char_height * num_rows
 
-    out_img = Image.new("RGB", (out_width, out_height), bg_code)
+    out_img = Image.new("RGB", (out_width, out_height), (255, 255, 255))
     draw = ImageDraw.Draw(out_img)
 
     for i in range(int(num_rows)):
         for j in range(int(num_cols)):
-            partial_image = image[int(i * cell_h):min(int((i + 1) * cell_h), height),
-                            int(j * cell_w):min(int((j + 1) * cell_w), width), :]
+            partial_image = image[int(i * cell_h):   min(int((i + 1) * cell_h), height),
+                            int(j * cell_w):   min(int((j + 1) * cell_w), width),
+                            :]
+
             partial_avg_color = np.sum(np.sum(partial_image, axis=0), axis=0) / (cell_h * cell_w)
             partial_avg_color = tuple(partial_avg_color.astype(np.int32).tolist())
+
             c = char_list[min(int((int(np.mean(partial_image)) * num_chars) / 255), num_chars - 1)]
+
             draw.text((j * char_width, i * char_height), c, fill=partial_avg_color, font=font)
 
-    if bg == "white":
-        cropped_img = ImageOps.invert(out_img).getbbox()
-    elif bg == "black":
-        cropped_img = out_img.getbbox()
-
+    cropped_img = out_img.getbbox()
     f_img = out_img.crop(cropped_img)
 
     return f_img
